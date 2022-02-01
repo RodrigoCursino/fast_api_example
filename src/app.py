a0a2f27b9621b8d5753d5d5620a72f9671f564b3
RodrigoCursino <rodrigoapmc@gmail.com>
2022-01-31 22:26:40 -0300

    teste deploy

--- src/app.py
+++ src/app.py
@@ -22,24 +22,25 @@ api.add_middleware(
 )
 
 # arquivos estáticos gerados pelo build
-api.mount("/js/", StaticFiles(directory="src/static/js"))
-api.mount("/css/", StaticFiles(directory="src/static/css"))
-api.mount("/img/", StaticFiles(directory="src/static/img"))   
-api.mount("/fonts/", StaticFiles(directory="src/static/fonts"))  
+# api.mount("/js/", StaticFiles(directory="src/static/js"))
+# api.mount("/css/", StaticFiles(directory="src/static/css"))
+# api.mount("/img/", StaticFiles(directory="src/static/img"))   
+# api.mount("/fonts/", StaticFiles(directory="src/static/fonts"))  
 # arquivos estáticos gerados pelo build
 
 # index build files
-templates = Jinja2Templates(directory="src/templates") 
+#templates = Jinja2Templates(directory="src/templates") 
 
 # Route Teams
-api.include_router(teams.router)
+# api.include_router(teams.router)
 # Route Users
-api.include_router(users.router)
+# api.include_router(users.router)
 # Route Auth
-api.include_router(auth.router)
+# api.include_router(auth.router)
 
 @api.get("/")
 async def serve_home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return {"message": "Heroko Hello world"}
+    #return templates.TemplateResponse("index.html", {"request": request})
 
 
